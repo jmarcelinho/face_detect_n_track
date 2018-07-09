3ed344c2af87fdb996071ba6fff92a67541b1815
Joao Marcelo <marcelinhofernandes2013@gmail.com>
2018-07-09 11:12:02 -0300

    using std::to_string

--- main.cpp
+++ main.cpp
@@ -9,22 +9,6 @@ using namespace cv;
 const String WINDOW_NAME("Camera video");
 const String CASCADE_FILE("haarcascade_frontalface_alt.xml");
 
-string toStr(int id)
-{
-	string prc = "";
-	if (id == 0)
-		return "0";
-	while (id != 0)
-	{
-		prc += '0' + (id % 10);
-		id /= 10;
-	}
-	string ret = "";
-	for (int i = prc.size() - 1; i >= 0; i--)
-		ret += prc[i];
-	return ret;
-}
-
 int main(int argc, char **argv)
 {
 	// Try opening camera
@@ -57,8 +41,8 @@ int main(int argc, char **argv)
 
 		if (detector.isFaceFound())
 		{
-			String path = toStr(id++);
-			imwrite("image/captureFace_" + path + ".png", frame(detector.face()), compression_params);
+			String path = "image/captureFace_" + to_string(id++) + ".png";
+			imwrite(path, frame(detector.face()), compression_params);
 			rectangle(frame, detector.face(), Scalar(255, 0, 0));
 			circle(frame, detector.facePosition(), 30, Scalar(0, 255, 0));
 		}
